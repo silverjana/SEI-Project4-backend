936ed57110339507f1c8d99764b7d506a4343d07
Arianna Giordano <silverjana@live.com>
2022-09-08 17:38:29 +0100

    auth views

--- jwt_auth/views.py
+++ jwt_auth/views.py
@@ -1,3 +1,72 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.exceptions import PermissionDenied
+from rest_framework import status
+
+from django.contrib.auth import get_user_model
+User = get_user_model() #dynamic
+
+import jwt
+from datetime import datetime, timedelta # datetime : get now as a date. timedelta: period of time as a num value. together:expiry date
+from django.conf import settings #import settings as var - SECRET_KEY
+
+from .serializers.common import UserSerializer
+
+class RegisterView(APIView):
+    
+  def post(self, request):
+
+    user_to_create = UserSerializer(data=request.data)
+
+    try:
+      user_to_create.is_valid(True) # pass request through the validate method in the serializer. If succeeds,adds the validated_data key to the user_to_create object
+      user_to_create.save() # save() then uses validated_data object to create a new user. Once successful, it will add a data key to user_to_create, which we can then send back to the user
+      return Response(user_to_create.data, status=status.HTTP_202_ACCEPTED)
+
+    except Exception as e:
+      print('error in registerview/post' )
+      return Response(e.__dict__ if e.__dict__ else str(e), status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+
+  
+class LoginView(APIView):
+
+    # 1. check that email on request.data matches a User record
+    # 2. does the plain text password on request.data match the hashes password stored in the database?
+    # 3. If user is validated -> send back a token
+    # 4. If not validated -> PermissionDenied response 
+
+  def post(self, request):
+    email = request.data.get('email')
+    password = request.data.get('password')
+
+    #1
+    try:
+      user_to_login = User.objects.get(email=email)
+    except User.DoesNotExist:
+      print("FAILED AT EMAIL STAGE")
+      raise PermissionDenied("Invalid credentials") #4
+
+    #2
+    if not user_to_login.check_password(password):  #if don't match -> error
+      print("FAILED AT PASSWORD STAGE")
+      raise PermissionDenied("Invalid credentials") #4  
+    
+    #3
+    dt = datetime.now() + timedelta(days=7) # dt will be a timestamp 7 days from now
+
+    token = jwt.encode(
+      # 1st argument: payload
+      {
+        "sub": user_to_login.id,
+        "exp": int(dt.strftime('%s'))    # dt time transformed in seconds and then integer
+      },
+      #2nd argument: secret -> settings var
+      settings.SECRET_KEY,
+      #3rd arg: algorythm (default one)
+      "HS256"
+    )
+    print("TOKEN IS HERE ->", token)
+
--- project/settings.py
+++ project/settings.py
@@ -86,6 +86,13 @@ DATABASES = {
 
 AUTH_USER_MODEL = 'jwt_auth.User'  
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'jwt_auth.authentication.JWTAuthentication'
+    ]
+}
+
+
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
